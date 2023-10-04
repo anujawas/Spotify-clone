2c0ba6d26287c14abd174527eae21d4228619116
anujawas <awasthianuj394@gmail.com>
2023-10-04 16:18:25 +0530

    Model Provider

---
+++ providers/ModelProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import AuthModel from "@/components/AuthModel";
+
+import { useEffect, useState } from "react"
+const ModelProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+    if (!isMounted) {
+        return null;
+    }
+    return (
+        <>
+            <AuthModel />
+        </>
+    )
+}
+
+export default ModelProvider;
