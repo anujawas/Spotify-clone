e3c1da6e474da0a86422bd38ce16023696f356a6
anujawas <awasthianuj394@gmail.com>
2023-10-10 20:02:48 +0530

    modified usePlayerHook to support suffle and repeat

--- hooks/usePlayer.ts
+++ hooks/usePlayer.ts
@@ -4,6 +4,10 @@ import { create } from "zustand";
 interface PlayerStore {
     ids: string[];
     activeId?: string;
+    isShuffle: boolean;
+    isRepeat: boolean;
+    setIsRepeat: (isRepeat: boolean) => void;
+    setIsShuffle: (isShuffle: boolean) => void;
     setId: (id: string) => void;
     setIds: (ids: string[]) => void;
     reset: () => void;
@@ -12,6 +16,10 @@ interface PlayerStore {
 const usePlayer = create<PlayerStore>((set) => ({
     ids: [],
     activeId: undefined,
+    isShuffle: false,
+    isRepeat: false,
+    setIsRepeat: (isRepeat: boolean) => set({ isRepeat: isRepeat }),
+    setIsShuffle: (isShuffle: boolean) => set({ isShuffle: isShuffle }),
     setId: (id: string) => set({ activeId: id }),
     setIds: (ids: string[]) => set({ ids: ids }),
     reset: () => set({ ids: [], activeId: undefined })
