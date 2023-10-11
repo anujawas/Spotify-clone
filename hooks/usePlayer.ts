8d119f77fa9bc82512d9d25e91793e8dc3b5f154
anujawas <awasthianuj394@gmail.com>
2023-10-11 10:35:02 +0530

    Added sldier value to usePlayer Hook

--- hooks/usePlayer.ts
+++ hooks/usePlayer.ts
@@ -3,9 +3,13 @@ import { create } from "zustand";
 
 interface PlayerStore {
     ids: string[];
+    currValue: number;
     activeId?: string;
     isShuffle: boolean;
     isRepeat: boolean;
+    isPaused: boolean;
+    setIsPaused: (isPaused: boolean) => void;
+    setCurrValue: (currValue: number) => void;
     setIsRepeat: (isRepeat: boolean) => void;
     setIsShuffle: (isShuffle: boolean) => void;
     setId: (id: string) => void;
@@ -18,6 +22,10 @@ const usePlayer = create<PlayerStore>((set) => ({
     activeId: undefined,
     isShuffle: false,
     isRepeat: false,
+    currValue: 0,
+    isPaused: false,
+    setIsPaused: (isPaused: boolean) => set({ isPaused: isPaused }),
+    setCurrValue: (currValue: number) => set({ currValue: currValue }),
     setIsRepeat: (isRepeat: boolean) => set({ isRepeat: isRepeat }),
     setIsShuffle: (isShuffle: boolean) => set({ isShuffle: isShuffle }),
     setId: (id: string) => set({ activeId: id }),
