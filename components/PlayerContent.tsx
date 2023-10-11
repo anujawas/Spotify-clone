562bb804d783ec46010a2b57d3d9c37923775361
anujawas <awasthianuj394@gmail.com>
2023-10-11 10:35:53 +0530

    upldated player componet to support repeat and shuffle

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -19,6 +19,7 @@ interface PlayerContentProps {
     song: Song;
     songUrl: string;
 }
+
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
     const player = usePlayer();
     const [volume, setVolume] = useState(1);
@@ -31,27 +32,42 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
         if (player.ids.length === 0) {
             return;
         }
+        player.setCurrValue(0);
         const currIndex = player.ids.findIndex((id) => id === player.activeId);
-        const nextSong = player.isRepeat ? currIndex.toString() : player.isShuffle ? (Math.ceil(Math.random() * player.ids.length) - 1).toString() : player.ids[currIndex + 1];
+        const randomIndex = (Math.ceil(Math.random() * player.ids.length)) - 1;
+        const nextSong = player.isRepeat ? player.ids[currIndex] : player.isShuffle ? player.ids[randomIndex === currIndex ? currIndex + 1 : randomIndex] : player.ids[currIndex + 1];
+
         if (!nextSong) {
             return player.setId(player.ids[0]);
         }
+        if (player.isRepeat) {
+            stop();
+            play();
+        }
         player.setId(nextSong);
+
     }
 
     const onPlayPrevious = () => {
+
         if (player.ids.length === 0) {
             return;
         }
+        player.setCurrValue(0);
         const currIndex = player.ids.findIndex((id) => id === player.activeId);
-        const prevSong = player.isRepeat ? currIndex.toString() : player.ids[currIndex - 1];
+        const prevSong = player.isRepeat ? player.ids[currIndex] : player.ids[currIndex - 1];
         if (!prevSong) {
             return player.setId(player.ids[player.ids.length - 1]);
         }
+        if (player.isRepeat) {
+            stop();
+            play();
+        }
         player.setId(prevSong);
+
     }
 
-    const [play, { pause, sound, duration }] = useSound<any>(
+    const [play, { stop, pause, sound, duration }] = useSound<any>(
         songUrl, {
         volume: volume,
         onplay: () => setIsPlaying(true),
@@ -100,9 +116,10 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
             setVolume(0);
         }
     }
+
     const RepeatIcon = player.isRepeat ? BsRepeat1 : BsRepeat;
     return (
-        <div className="grid grid-cols-2 md:grid-cols-3 h-full">
+        <div className="grid grid-cols-2 md:grid-cols-3 h-full" >
             <div className="flex w-full justify-start">
                 <div className="flex items-center gap-x-4">
                     <MediaItem data={song} />
@@ -139,7 +156,8 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
                         onClick={handleRepeat}
                     />
                 </div>
-                <AudioSlider duration={duration} isPlaying={isPlaying} />
+                <AudioSlider duration={duration} isPlaying={isPlaying}
+                />
             </div>
 
             <div className="hidden md:flex w-full justify-end pr-2">
