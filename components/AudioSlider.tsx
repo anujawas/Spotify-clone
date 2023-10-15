4750faf02cc91d58062bd1cdb8c360c59324c400
anujawas <awasthianuj394@gmail.com>
2023-10-15 21:10:53 +0530

    fixed overshooting bug

--- components/AudioSlider.tsx
+++ components/AudioSlider.tsx
@@ -15,6 +15,10 @@ const AudioSlider: React.FC<AudioSliderProps> = ({ duration, isPlaying }) => {
             player.setCurrValue(player.currValue + 0.2);
         }, 200);
     }
+
+    if (duration != null && Math.floor(player.currValue) >= Math.floor(duration / 1000)) {
+        player.setCurrValue(0);
+    }
     const toHHMMSS = (sec_num: number) => {
         var hours = Math.floor(sec_num / 3600);
         var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
@@ -36,7 +40,7 @@ const AudioSlider: React.FC<AudioSliderProps> = ({ duration, isPlaying }) => {
         <div className="w-full h-full flex items-center gap-x-2 justify-center hover-div">
             <p className="text-sm font-bold text-white">{!duration ? "00:00" : toHHMMSS(Math.floor(player.currValue))}</p>
             <input placeholder="audio-range" type={'range'} value={player.currValue} min={0} step={1} max={!duration ? 100 : Math.ceil(duration / 1000)} id="progress" readOnly />
-            <p className="text-sm font-bold text-white">{!duration ? "00:00" : toHHMMSS(Math.ceil(duration / 1000) - 5)}</p>
+            <p className="text-sm font-bold text-white">{!duration ? "00:00" : toHHMMSS(Math.floor(duration / 1000) - 5)}</p>
         </div>
     );
 }
